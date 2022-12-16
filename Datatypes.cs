749d6881fd28132e3377f2f564734e1a9a148c7a
ronitshinde <rony.shinde753@gmail.com>
2022-12-16 23:01:43 +0530

    All datatypes

---
+++ Datatypes.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Hello_World
+{
+    public class Datatypes
+    {
+
+        public static void Variousdatatypes()
+        {
+
+            int value = 11;
+
+            double value1 = 21.45;
+
+            char value2 = 'R';
+
+            bool value3 = true;
+
+            string value4 = "Rony";
+
+            Console.WriteLine("Integer value " + value);
+
+            Console.WriteLine("Double value " + value1);
+
+            Console.WriteLine("Char value " + value2);
+
+            Console.WriteLine("Boolean value " + value3);
+
+            Console.WriteLine("String value " + value4);
+
+            Console.WriteLine("Enter the username ");
+
+            String username = Console.ReadLine();
+
+            Console.WriteLine("The value given by user is " + username);
+            Console.ReadLine();
+        }
+    }
+}
