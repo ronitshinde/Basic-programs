74c54424d6ef376e3f30195e40247949715caa66
ronitshinde <rony.shinde753@gmail.com>
2022-12-16 22:55:29 +0530

    Addition basic program

---
+++ Addition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Hello_World
+{       
+    internal class Addition
+    {
+
+        public static void Incrementdecrement()
+        {
+                        
+            Console.WriteLine("First number is");
+
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Second number is");
+
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+            
+            
+            int Number3 = Number1 + Number2;
+           
+            Console.WriteLine("The addition of two Numbers : " + Number3);
+
+
+        }
+    }
+}
+
+
+
+
+   
